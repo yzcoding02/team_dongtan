bcdb7d37d38ee4a67d73137e7ef285e8899748d5
dreamliketwo <117730615+dreamliketwo@users.noreply.github.com>
2022-11-08 20:50:27 +0900

    진우:what's up you *****

---
+++ Jinwoo/7.js
@@ -0,0 +1,46 @@
+//1번
+// var sum = 0
+// for (var i = 100; i < 121; i++) {
+//     if (i % 5 == 0) {
+//         sum += i
+//     }
+// }
+// console.log(sum)
+//2번
+// var sum = 1
+// var su = 0
+// for (var i = 100; i < 121; i++) {
+//     if (i % 7 == 0) {
+//         sum *= i
+//     } else {
+//         su += i
+//     }
+// }
+// console.log(sum - su)
+//3번
+// function 원뿔의부피(밑넓이, 높이) {
+//     var mul = 밑넓이 * 밑넓이 * 3.14 * 높이 / 3;
+//     console.log(mul)
+// }
+// 원뿔의부피(2, 3)
+//4번
+// for (var i = 1; i < 51; i++) {
+//     if (i % 3 == 0) {
+//         if (i % 4 == 0) {
+//             console.log(i)
+//         }
+//     }
+// }
+
+//5번
+function 홀수(갯수)
+//6번
+function 큰수(숫자1, 숫자2) {
+    if (숫자1 > 숫자2) {
+
+
+
+
+
+    }
+}
