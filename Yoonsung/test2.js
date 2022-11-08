6e160af9aa62452ac1e74564acf474fe07f6b9c6
Regocoriander <117729434+Regocoriander@users.noreply.github.com>
2022-11-08 20:48:14 +0900

    Add files via upload

---
+++ Yoonsung/test2.js
@@ -0,0 +1,38 @@
+//for(var i=1;i<11; i++)b
+//    console.log(i*1);
+//var sum=0;
+//for(var i=1;i<6; i++){
+//    sum+=i;
+//    console.log(sum);
+//}
+//var sum=1;
+//for(var i=1;i<6; i++){
+ //   sum*=i;
+//    console.log(sum);
+//}
+//var sum=-5;
+//for(var i=1;i<6; i++){
+ //   sum+=i;
+ //   console.log(sum);
+//}
+var sum=0;
+for(var i=1;i<1001; i++){
+    if(i%2==0){
+        sum+=i;    
+    }
+}
+console.log(sum);
+B
+var sum=0;
+for(var i=1;i<1001; i++){
+    if(i%2!=0){
+        sum+=i;     
+    }
+}
+console.log(sum);
+
+var sum=0;
+for(var i=1;i<1001; i++){
+    sum+=i;     
+}
+console.log(sum);
