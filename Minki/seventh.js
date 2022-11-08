618d8b27b7e766c1d463cd49589d9b1671213e74
1972dream <117729214+1972dream@users.noreply.github.com>
2022-11-08 20:47:23 +0900

    seven.js

---
+++ Minki/seventh.js
@@ -0,0 +1,49 @@
+// 1번
+// var sum = 0
+// for (var i = 100; i < 121; i++) {
+//     if (i % 5 == 0) {
+//         sum += i
+
+
+//     }
+// }
+// console.log(sum);
+// 2번
+// sum = 1
+// su33 = 0
+// for (var i = 100; i < 121; i++) {
+//     if (i % 7 == 0) {
+//         sum *= i
+//     } else {
+//         su33 += i
+//     }
+// }
+// console.log(sum - su33)
+// 3번
+// function cone(x, y) {
+//     var mul = x * x * 3.14 * y * 1 / 3;
+
+//     console.log(mul);
+// }
+// cone(2, 3);
+
+// 4번
+// for (var i = 1; i < 51; i++) {
+//     if (i % 3 == 0) {
+
+//         if (i % 4 == 0) {
+//             console.log(i)
+//         }
+
+
+//     }
+// }
+// 5번
+skip
+// 6번
+
+10
+// 7번
+skip
+// 8번                                              
+skip
