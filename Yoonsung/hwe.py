d30607855d359598cc13c6776d45c5c3ac58d5b3
Regocoriander <limsso06545@gmail.com>
2023-01-05 20:21:26 +0900

    까먹었다

---
+++ Yoonsung/hwe.py
@@ -0,0 +1,23 @@
+#1. for i in range(11):
+#     print("나는숙제중립니다")
+
+#2. for i in range(30,41):
+    # print(i)
+
+#3. sum=0
+# for i in range(1,21):
+#     if i%5==0:
+#         sum+=i
+# print(sum)
+
+#4. sum=10
+# for i in range(10,21):
+#      if i%3==0:
+#         sum*=i
+# print(sum)
+
+#5. sum=0
+# for i in range(100,201):
+#      if i%5==0:
+#        if i%7==0:
+#             print(i)
