7172b4f23b4c5dd8ad913cec15811913423628c6
yoohyeon5364 <tvgguldeocki@gmail.com>
2023-01-10 20:06:31 +0900

    Day3 hw

---
+++ Doyoon/python_HW/Day3_hw.py
@@ -0,0 +1,22 @@
+for i in range(10):
+    print("난 숙제 중 입니다.")
+
+i = 30
+while i < 41:
+    print(i)
+    i += 1
+
+# 2
+sum = 1
+for i in range(1, 20):
+    if i % 5 == 0:
+        sum += i
+        print(i)
+
+i = 1
+sum = 1
+while i < 20:
+    i += 1
+    if i % 5 == 0:
+        sum = +i
+    print(sum)
