55f861480ad43d58b1f423ddd0f3d7a7f82b2b3b
yoohyeon5364 <tvgguldeocki@gmail.com>
2023-01-17 19:11:45 +0900

    숙제

--- Doyoon/python_HW/Day3_hw.py
+++ Doyoon/python_HW/Day3_hw.py
@@ -13,10 +13,27 @@ for i in range(1, 20):
         sum += i
         print(i)
 
+# 3
+sum = 0
 i = 1
-sum = 1
-while i < 20:
-    i += 1
+while i <= 20:
     if i % 5 == 0:
-        sum = +i
-    print(sum)
+        sum += i
+    i += 1
+print(sum)
+
+# 4
+num = 1
+i = 10
+while i <= 20:
+    if i % 3 == 0:
+        num *= i
+    i += 1
+print(num)
+
+# 5
+i = 100
+while i <= 200:
+    if i % 35 == 0:
+        print(i)
+    i += 1
