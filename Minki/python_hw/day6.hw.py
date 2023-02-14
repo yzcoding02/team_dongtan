48530658c9c8d28eef22640e6a88a0c49d77aa57
1972dream <ming74.kim@gmail.com>
2023-02-14 18:06:39 +0900

    day6.hw

--- Minki/python_hw/day6.hw.py
+++ Minki/python_hw/day6.hw.py
@@ -1,10 +1,35 @@
 # 1에서 20중 3의 배수를
 # 리스트로 담아서 내림차순으로 출력
-민기 = []
-for i in range(1, 21):
-    if i % 3 == 0:
+# 민기 = []
+# for i in range(1, 21):
+#     if i % 3 == 0:
 
-        민기.append(i)
+#         민기.append(i)
 
-민기.reverse()
-print(민기)
+# 민기.reverse()
+# print(민기)
+
+
+# 숙제\
+# append
+# 민기 = []
+# 민기.append("와샌즈")
+# print(민기)
+# pop
+# 민기 = []
+# 민기.append("와샌즈")
+# 민기.pop()
+# print(민기)
+# remove
+# 민기 = ["와샌즈", "아시는", "구나"]
+# 민기.remove("와샌즈")
+# print(민기)
+# sort
+# num = [2, 4, 1, 3]
+# print(num)
+# num.sort()
+# print(num)
+# reverse
+# num = [2, 4, 1, 3]
+# num.reverse()
+# print(num)
